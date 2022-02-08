24029fe3673749cf6f4517c6700c090970529e00
Tamas Janos Foldi <tamas_janos.foldi@lidl.hu>
2022-02-08 17:34:49 +0100

    Add projects route

--- frontend/app/src/app/app-routing.module.ts
+++ frontend/app/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'projects',
+    loadChildren: () =>
+      import('./projects-page/projects-page.module').then(
+        (m) => m.ProjectsPageModule
+      ),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'projects',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
