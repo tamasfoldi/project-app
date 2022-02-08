54834ab5dc46692a0bebd1a777e67037ab13da38
Tamas Janos Foldi <tamas_janos.foldi@lidl.hu>
2022-02-08 16:46:20 +0100

    Add effect for project fetching

--- frontend/app/src/app/project/project.effects.spec.ts
+++ frontend/app/src/app/project/project.effects.spec.ts
@@ -1,25 +1,56 @@
-import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 import { Observable } from 'rxjs';
+import {
+  SpectatorService,
+  createServiceFactory,
+  SpyObject,
+} from '@ngneat/spectator';
+import { cold, hot } from 'jasmine-marbles';
 
 import { ProjectEffects } from './project.effects';
+import { Project, ProjectService } from '../../api';
+import { fetchProjects, loadProjects } from './project.actions';
 
 describe('ProjectEffects', () => {
   let actions$: Observable<any>;
   let effects: ProjectEffects;
+  let spectator: SpectatorService<ProjectEffects>;
+  let projectService: SpyObject<ProjectService>;
+
+  const createService = createServiceFactory<ProjectEffects>({
+    service: ProjectEffects,
+    providers: [provideMockActions(() => actions$)],
+    mocks: [ProjectService],
+  });
 
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        ProjectEffects,
-        provideMockActions(() => actions$)
-      ]
-    });
-
-    effects = TestBed.inject(ProjectEffects);
+    spectator = createService();
+    effects = spectator.service;
+    projectService = spectator.inject(ProjectService);
   });
 
   it('should be created', () => {
     expect(effects).toBeTruthy();
+  });
+
+  describe('fetchProjects$', () => {
+    it('should return a loadProjects action', () => {
+      const projects: Project[] = [
+        {
+          id: 'project',
+          title: 'Test Project',
+          published_at: '2021-01-01',
+        },
+      ];
+      const action = fetchProjects();
+      const completion = loadProjects({ projects });
+
+      actions$ = hot('-a---', { a: action });
+      const response = cold('-a|', { a: projects });
+      const expected = cold('--b', { b: completion });
+      projectService.projectsGet.and.returnValue(response);
+
+      expect(spectator.service.fetchProjects$).toBeObservable(expected);
+    });
   });
 });
--- frontend/app/src/app/project/project.effects.ts
+++ frontend/app/src/app/project/project.effects.ts
@@ -1,13 +1,24 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
-
-
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { map, mergeMap } from 'rxjs/operators';
+import { Project, ProjectService } from '../../api';
+import { fetchProjects, loadProjects } from './project.actions';
 
 @Injectable()
 export class ProjectEffects {
+  fetchProjects$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(fetchProjects),
+      mergeMap(() =>
+        this.projectService
+          .projectsGet()
+          .pipe(map((projects: Project[]) => loadProjects({ projects })))
+      )
+    )
+  );
 
-
-
-  constructor(private actions$: Actions) {}
-
+  constructor(
+    private actions$: Actions,
+    private projectService: ProjectService
+  ) {}
 }
